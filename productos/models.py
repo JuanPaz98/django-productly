3c395019fe6bbd6797b2b181dca1209b86813346
Juan Paz <jjfer032@gmail.com>
2025-09-17 15:59:05 -0500

    adding models

--- productos/models.py
+++ productos/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Categoria(models.Model):
+    id = models.IntegerField()
+    nombre = models.CharField(max_length=255),
+
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=255)
+    stock = models.IntegerField()
+    puntaje = models.FloatField()
+    Categoria = models.ForeignKey(Categoria.id, on_delete=models.CASCADE)
